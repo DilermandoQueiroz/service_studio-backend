ba54aca458f2971394c973f42fd06995f725535d
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-14 00:56:06 -0300

    added sell

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -32,3 +32,23 @@ class Client(BaseModel):
     complement: Union[str, None] = None 
     email: Union[str, None] = None 
     phone_number: Union[str, None] = None
+
+class Sell(BaseModel):
+     service_provider: str
+     studio: Union[str, None] = None
+     client: str
+     service_style: Union[int, None] = None
+     tender: Union[int, None] = None
+     price: int
+     studio_rate: Union[int, None] = None
+     client_rate: Union[int, None] = None
+     service_provider_rate: Union[int, None] = None
+     client_suggestion_desc: Union[str, None] = None
+     client_satisfied: Union[bool, None] = None
+     number_of_sessions: int
+     client_contract_confirmed: Union[bool, None] = None
+     service_provider_contract_confirmed: Union[bool, None] = None
+     start_time: str
+     last_update: Union[str, None] = None
+     finish_time: Union[str, None] = None 
+     phone_number: Union[str, None] = None 
