3ad09b485dc947edf2ffa6e37f28b497082409d5
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-16 17:51:56 -0300

    added new routes

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 
 from sqlalchemy.orm import Session
@@ -30,8 +30,17 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/provider/create", response_model = schemas.ServiceProvider)
-def create_service_provider(service_provider: schemas.ServiceProvider, db: Session = Depends(get_db)):
+@app.post("/studio/create", response_model = schemas.StudioCreate, status_code = status.HTTP_201_CREATED)
+def create_studio_provider(studio: schemas.StudioCreate, db: Session = Depends(get_db)):
+    db_studio_name = crud.get_studio_by_name(db=db, name=studio.name)
+    
+    if db_studio_name:
+        raise HTTPException(status_code=400, detail="Name already registered")
+    
+    return crud.create_studio(db=db, studio=studio)
+
+@app.post("/provider/create", response_model = schemas.ServiceProviderCreate, status_code = status.HTTP_201_CREATED)
+def create_service_provider(service_provider: schemas.ServiceProviderCreate, db: Session = Depends(get_db)):
     db_service_provider_cpf = crud.get_service_provider_by_cpf(db=db, cpf=service_provider.cpf)
     db_service_provider_name = crud.get_service_provider_by_name(db=db, name=service_provider.name)
     
@@ -42,8 +51,46 @@ def create_service_provider(service_provider: schemas.ServiceProvider, db: Sessi
 
     return crud.create_service_provider(db=db, service_provider=service_provider)
 
-# TODO: What is to return?
-@app.get("/provider/delete")
+@app.post("/client/create", response_model = schemas.ClientCreate, status_code = status.HTTP_201_CREATED)
+def create_client(client: schemas.ClientCreate, db: Session = Depends(get_db)):
+    db_client_cpf = crud.get_client_by_cpf(db=db, cpf=client.cpf)
+    db_client_name = crud.get_client_by_name(db=db, name=client.name)
+
+    if db_client_name:
+        raise HTTPException(status_code=400, detail="Name already registered")
+    elif db_client_cpf:
+        raise HTTPException(status_code=400, detail="Cpf already registered")
+
+    return crud.create_client(db=db, client=client)
+
+@app.post("/sell/create", response_model=schemas.SellCreate, status_code = status.HTTP_201_CREATED)
+def create_sell(sell: schemas.SellCreate, db: Session = Depends(get_db)):
+    db_service_provider = crud.get_service_provider_by_name(db=db, name=sell.service_provider_name)
+    db_client = crud.get_client_by_name(db=db, name=sell.client_name)
+
+    if sell.studio_name:
+        db_studio = crud.get_studio_by_name(db=db, name=sell.studio_name)
+
+        if not db_studio:
+            raise HTTPException(status_code=400, detail="This studio not exists")
+
+    if not db_service_provider:
+        raise HTTPException(status_code=400, detail="This service provider not exists")
+    elif not db_client:
+        raise HTTPException(status_code=400, detail="This client not exists")
+
+    return crud.create_sell(db=db, sell=sell)
+
+@app.get("/studio/remove")
+def remove_studio_by_name(name: str = None, db: Session = Depends(get_db)):
+    response = crud.delete_studio_by_name(db=db, name=name)
+
+    if not response:
+        raise HTTPException(status_code=400, detail="Name not exists")
+    
+    return f"{name} deleted"
+
+@app.get("/provider/remove")
 def remove_service_provider_by_name(name: str = None, db: Session = Depends(get_db)):
     response = crud.delete_service_provider_by_name(db=db, name=name)
 
@@ -52,9 +99,27 @@ def remove_service_provider_by_name(name: str = None, db: Session = Depends(get_
     
     return f"{name} deleted"
 
-@app.get("/providers", response_model = List[schemas.ServiceProvider])
-def read_service_provider(db: Session = Depends(get_db)):
+# TODO: What is to return?
+@app.get("/client/remove")
+def remove_client_by_name(name: str = None, db: Session = Depends(get_db)):
+    response = crud.delete_client_by_name(db=db, name=name)
+
+    if not response:
+        raise HTTPException(status_code=400, detail="Name not exists")
+    
+    return f"{name} deleted"
+
+@app.get("/studios", response_model=List[schemas.Studio])
+def read_studios(db: Session = Depends(get_db)):
+    return crud.get_studio(db)
+
+@app.get("/providers", response_model=List[schemas.ServiceProvider])
+def read_service_providers(db: Session = Depends(get_db)):
     return crud.get_service_provider(db)
+
+@app.get("/clients")
+def read_clients(db: Session = Depends(get_db)):
+    return crud.get_client(db)
 
 @app.get("/provider/", response_model = schemas.ServiceProvider)
 def read_service_provider_by(name: str = None, cpf: str = None,db: Session = Depends(get_db)):
@@ -68,28 +133,11 @@ def read_service_provider_by(name: str = None, cpf: str = None,db: Session = Dep
     
     return db_service_provider
 
-@app.post("/client/create")
-def create_client(client: schemas.Client, db: Session = Depends(get_db)):
-    db_client_cpf = crud.get_client_by_cpf(db=db, cpf=client.cpf)
-    db_client_name = crud.get_client_by_name(db=db, name=client.name)
+@app.get("/client/", response_model=schemas.Client)
+def read_client_by_name(name: str = None, db: Session = Depends(get_db)):
+    db_client = crud.get_client_by_name(db=db, name=name)
 
-    if db_client_name:
-        raise HTTPException(status_code=400, detail="Name already registered")
-    elif db_client_cpf:
-        raise HTTPException(status_code=400, detail="Cpf already registered")
-
-    return crud.create_client(db=db, client=client)
-
-# TODO: What is to return?
-@app.get("/client/remove")
-def remove_client_by_name(name: str = None, db: Session = Depends(get_db)):
-    response = crud.delete_client_by_name(db=db, name=name)
-
-    if not response:
-        raise HTTPException(status_code=400, detail="Name not exists")
+    if db_client is None:
+        raise HTTPException(status_code=404, detail="Service provider not found")
     
-    return f"{name} deleted"
-
-@app.get("/clients")
-def read_client(db: Session = Depends(get_db)):
-    return crud.get_client(db)
+    return db_client
