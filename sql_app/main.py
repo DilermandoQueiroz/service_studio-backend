71b6f603c43843804764180cf062eb2e7f8cff42
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-24 01:18:35 -0300

    corretions in use of schemas and crud

--- sql_app/main.py
+++ sql_app/main.py
@@ -5,13 +5,13 @@ from fastapi.middleware.cors import CORSMiddleware
 import firebase_admin
 from firebase_admin import auth, credentials
 from sqlalchemy.orm import Session
-import crud, models, schemas
-from database import SessionLocal, engine
+import crud, schemas
+from database import SessionLocal, engine, Base
 import custom_logger as logging
 
 cred = credentials.Certificate('shared/firebase-private-key.json')
 firebase_app = firebase_admin.initialize_app(cred)
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
@@ -39,142 +39,160 @@ def validate_token(header_autorization: string):
         bearer_token = header_autorization.split(" ")[1]
         token_verified = auth.verify_id_token(bearer_token, app=firebase_app)
         if token_verified['email_verified'] == False:
-            raise HTTPException(status_code=401, detail="Email not verified")
+            return False
         return True
-    except Exception as e:
-        logger.error(e)
+    except Exception:
         raise HTTPException(status_code=401, detail="Invalid Token")
-
 
 @app.post("/studio/create", response_model = schemas.StudioCreate, status_code = status.HTTP_201_CREATED)
 def create_studio_provider(request: Request, studio: schemas.StudioCreate, db: Session = Depends(get_db)):
-    
     try:
         if validate_token(request.headers['authorization']):
-            db_studio_name = crud.get_studio_by_name(db=db, name=studio.name)
-            db_studio_owner_email = crud.get_studio_by_email(db=db, email=studio.email_owner)
+            db_studio_name = crud.studio.get_by_name(db=db, name=studio.name)
+            db_studio_owner_email = crud.studio.get_by_email(db=db, email=studio.email_owner)
+
             if db_studio_name:
                 raise HTTPException(status_code=400, detail="Name already registered")
             elif db_studio_owner_email:
                 raise HTTPException(status_code=400, detail="Owner email already registered")
 
-            return crud.create_studio(db=db, studio=studio)
-    except Exception as e:
-        logger.error(e)
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+            return crud.studio.create(db=db, obj_in=studio)
+        else:
+            raise HTTPException(status_code=401, detail="Email not verified")
+    except Exception as error:
+        logger.error(error)
+        raise error
 
 @app.post("/provider/create", response_model = schemas.ServiceProviderCreate, status_code = status.HTTP_201_CREATED)
 def create_service_provider(service_provider: schemas.ServiceProviderCreate, db: Session = Depends(get_db)):
-    db_service_provider_cpf = crud.get_service_provider_by_cpf(db=db, cpf=service_provider.cpf)
-    db_service_provider_name = crud.get_service_provider_by_name(db=db, name=service_provider.name)
-    db_service_provider_email = crud.get_service_provider_by_email(db=db, email=service_provider.email)
+    try:
+        db_service_provider_cpf = crud.provider.get_by_cpf(db=db, cpf=service_provider.cpf)
+        db_service_provider_name = crud.provider.get_by_name(db=db, name=service_provider.name)
+        db_service_provider_email = crud.provider.get_by_email(db=db, email=service_provider.email)
+        exceptions = []
 
-    if db_service_provider_name:
-        raise HTTPException(status_code=400, detail="Name already registered")
-    elif db_service_provider_cpf:
-        raise HTTPException(status_code=400, detail="Cpf already registered")
-    elif db_service_provider_email:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        if db_service_provider_name:
+            exceptions.append("name")
+        if db_service_provider_cpf:
+            exceptions.append("cpf")
+        if db_service_provider_email:
+            exceptions.append("email")
 
-    return crud.create_service_provider(db=db, service_provider=service_provider)
+        if len(exceptions) > 0:
+            raise HTTPException(status_code=400, detail=f"{', '.join(exceptions)} already registered")
+
+        return crud.provider.create(db=db, obj_in=service_provider)
+    except Exception as error:
+        logger.error(error)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.post("/client/create", response_model = schemas.ClientCreate, status_code = status.HTTP_201_CREATED)
 def create_client(client: schemas.ClientCreate, db: Session = Depends(get_db)):
-    db_client_cpf = crud.get_client_by_cpf(db=db, cpf=client.cpf)
-    db_client_name = crud.get_client_by_name(db=db, name=client.name)
-    db_client_email = crud.get_client_by_email(db=db, email=client.email)
+    # TODO: validate uid - token 
+    db_client_cpf = crud.client.get_by_cpf(db=db, cpf=client.cpf)
+    db_client_name = crud.client.get_by_name(db=db, name=client.name)
+    db_client_email = crud.client.get_by_email(db=db, email=client.email)
+    exceptions = []
 
     if db_client_name:
-        raise HTTPException(status_code=400, detail="Name already registered")
-    elif db_client_cpf:
-        raise HTTPException(status_code=400, detail="Cpf already registered")
-    elif db_client_email:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        exceptions.append("name")
+    if db_client_cpf:
+        exceptions.append("cpf")
+    if db_client_email:
+        exceptions.append("email")
 
-    return crud.create_client(db=db, client=client)
+    if len(exceptions) > 0:
+        raise HTTPException(status_code=400, detail=f"{', '.join(exceptions)} already registered")
+
+    return crud.client.create(db=db, obj_in=client)
 
 @app.post("/sell/create", response_model=schemas.SellCreate, status_code = status.HTTP_201_CREATED)
 def create_sell(sell: schemas.SellCreate, db: Session = Depends(get_db)):
-    db_service_provider = crud.get_service_provider_by_name(db=db, name=sell.service_provider_name)
-    db_client = crud.get_client_by_name(db=db, name=sell.client_name)
+    db_service_provider = crud.provider.get_by_name(db=db, name=sell.service_provider_name)
+    db_client = crud.client.get_by_name(db=db, name=sell.client_name)
+    exceptions = []
 
     if sell.studio_name:
-        db_studio = crud.get_studio_by_name(db=db, name=sell.studio_name)
+        db_studio = crud.studio.get_by_name(db=db, name=sell.studio_name)
 
         if not db_studio:
-            raise HTTPException(status_code=400, detail="This studio not exists")
+            exceptions.append("studio")
 
     if not db_service_provider:
-        raise HTTPException(status_code=400, detail="This service provider not exists")
-    elif not db_client:
-        raise HTTPException(status_code=400, detail="This client not exists")
+        exceptions.append("service provider")
+    if not db_client:
+        exceptions.append("client")
 
-    return crud.create_sell(db=db, sell=sell)
+    if len(exceptions) > 0:
+        raise HTTPException(status_code=400, detail=f"{', '.join(exceptions)} not exists")
+
+    return crud.sell.create(db=db, obj_in=sell)
 
 @app.get("/studio/remove")
 def remove_studio_by_name(name: str = None, db: Session = Depends(get_db)):
-    response = crud.delete_studio_by_name(db=db, name=name)
+    response = crud.studio.remove_by_name(db=db, name=name)
 
     if not response:
         raise HTTPException(status_code=400, detail="Name not exists")
     
-    return f"{name} deleted"
+    return response
 
 @app.get("/provider/remove")
 def remove_service_provider_by_name(name: str = None, db: Session = Depends(get_db)):
-    response = crud.delete_service_provider_by_name(db=db, name=name)
+    response = crud.provider.remove_by_name(db=db, name=name)
 
     if not response:
         raise HTTPException(status_code=400, detail="Name not exists")
     
-    return f"{name} deleted"
+    return response
 
-# TODO: What is to return?
 @app.get("/client/remove")
 def remove_client_by_name(name: str = None, db: Session = Depends(get_db)):
-    response = crud.delete_client_by_name(db=db, name=name)
+    response = crud.client.remove_by_name(db=db, name=name)
 
     if not response:
         raise HTTPException(status_code=400, detail="Name not exists")
     
-    return f"{name} deleted"
+    return response
 
-@app.get("/studios", response_model=List[schemas.Studio])
+@app.get("/studios", response_model=List[schemas.StudioInDBBase])
 def read_studios(db: Session = Depends(get_db)):
-    return crud.get_studio(db)
+    return crud.studio.get_all(db)
 
-@app.get("/providers", response_model=List[schemas.ServiceProvider])
+@app.get("/providers", response_model=List[schemas.ServiceProviderInDBBase])
 def read_service_providers(db: Session = Depends(get_db)):
-    return crud.get_service_provider(db)
+    return crud.provider.get_all(db)
 
-@app.get("/clients", response_model=List[schemas.Client])
+@app.get("/clients", response_model=List[schemas.ClientInDBBase])
 def read_clients(db: Session = Depends(get_db)):
-    return crud.get_client(db)
+    return crud.client.get_all(db)
 
-@app.get("/sells", response_model=List[schemas.Sell])
+@app.get("/sells", response_model=List[schemas.SellInDBBase])
 def read_sells(db: Session = Depends(get_db)):
-    return crud.get_sell(db)
+    return crud.sell.get_all(db)
 
-@app.get("/provider/", response_model = schemas.ServiceProvider)
+@app.get("/provider/", response_model = schemas.ServiceProviderInDBBase)
 def read_service_provider_by(name: str = None, cpf: str = None, email: str = None, db: Session = Depends(get_db)):
-    if name:
-        db_service_provider = crud.get_service_provider_by_name(db=db, name=name)
-    elif email:
-        db_service_provider = crud.get_service_provider_by_email(db=db, email=email)
-    elif cpf:
-        db_service_provider = crud.get_service_provider_by_cpf(db=db, cpf=cpf)
+    db_service_provider = False
 
-    if db_service_provider is None:
+    if name:
+        db_service_provider = crud.provider.get_by_name(db=db, name=name)
+    elif email:
+        db_service_provider = crud.provider.get_by_email(db=db, email=email)
+    elif cpf:
+        db_service_provider = crud.provider.get_by_cpf(db=db, cpf=cpf)
+
+    if not db_service_provider:
         raise HTTPException(status_code=404, detail="Service provider not found")
     
     return db_service_provider
 
-@app.get("/client/", response_model=schemas.Client)
+@app.get("/client/", response_model=schemas.ClientInDBBase)
 def read_client_by_name(name: str = None, email: str = None, db: Session = Depends(get_db)):
     if name:
-        db_client = crud.get_client_by_name(db=db, name=name)
+        db_client = crud.client.get_by_name(db=db, name=name)
     elif email:
-        db_client = crud.get_client_by_email(db=db, email=email)
+        db_client = crud.client.get_by_email(db=db, email=email)
     if db_client is None:
         raise HTTPException(status_code=404, detail="Service provider not found")
     
