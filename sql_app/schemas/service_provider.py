e9388c5006eceb97c6e85b7ea19484edd50bda0a
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-25 02:48:03 -0300

    new class for receive firebase and db

--- sql_app/schemas/service_provider.py
+++ sql_app/schemas/service_provider.py
@@ -1,19 +1,27 @@
+from datetime import date
 from typing import Optional
 
-from pydantic import BaseModel, validator, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, validator
 
-class ServiceProviderCreate(BaseModel):
-    name: str = Field(..., max_length=36)
+
+class BaseServiceProvider(BaseModel):
+    # TODO: validate phone number E.164
     display_name: str = Field(..., max_length=36)
-    cpf: str = Field(..., max_length=11)
     email: EmailStr
     phone_number: Optional[str] = Field(None, max_length=20)
-    signal: Optional[int] = Field(None, ge=0)
+
+class ServiceProviderFireBase(BaseServiceProvider):
+    password: str
+
+class ServiceProviderCreate(BaseServiceProvider):
+    birth_date: date
+    cpf: str = Field(..., max_length=11)
+    name: str = Field(..., max_length=36)
     description: Optional[str] = Field(None, max_length=255)
 
     class Config:
         orm_mode = True
-    
+
     @validator("cpf")
     def validator_cpf(cls, v):
         # TODO: validate cpf
@@ -22,3 +30,12 @@ class ServiceProviderCreate(BaseModel):
 class ServiceProviderInDBBase(ServiceProviderCreate):
     id: int
     email: str
+
+class ServiceProviderAll(BaseModel):
+    birth_date: date
+    display_name: str = Field(..., max_length=36)
+    email: EmailStr
+    phone_number: Optional[str] = Field(None, max_length=20)
+    password: str
+    cpf: str = Field(..., max_length=11)
+    description: Optional[str] = Field(None, max_length=255)
