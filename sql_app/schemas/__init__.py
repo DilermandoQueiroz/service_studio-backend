4a817952cfced5e0e98bf45b888111f2513fb7cd
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-25 02:47:46 -0300

    update

--- sql_app/schemas/__init__.py
+++ sql_app/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .studio import StudioCreate, StudioInDBBase
-from .service_provider import ServiceProviderCreate, ServiceProviderInDBBase
+from .service_provider import ServiceProviderCreate, ServiceProviderInDBBase, ServiceProviderFireBase, ServiceProviderAll
 from .client import ClientCreate, ClientInDBBase
 from .sell import SellCreate, SellInDBBase
