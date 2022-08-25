f79d41115dab1b9e42624ab48f1759633edb565d
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-25 12:48:45 -0300

    organize import

--- sql_app/schemas/__init__.py
+++ sql_app/schemas/__init__.py
@@ -1,4 +1,7 @@
 from .studio import StudioCreate, StudioInDBBase
-from .service_provider import ServiceProviderCreate, ServiceProviderInDBBase, ServiceProviderFireBase, ServiceProviderAll
+from .service_provider import (
+    ServiceProviderCreate, ServiceProviderInDBBase,
+    ServiceProviderFireBase, ServiceProviderAll
+)
 from .client import ClientCreate, ClientInDBBase
 from .sell import SellCreate, SellInDBBase
