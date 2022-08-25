e78ed1882c9834d5024ea1fb2b085e059c67169a
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-25 12:48:02 -0300

    organize import

--- sql_app/__init__.py
+++ sql_app/__init__.py
@@ -1,2 +1,6 @@
 from .main import app
 from .database import Base
+from .firebase_utils import (
+    create_service_provider_firebase, delete_by_user_uid,
+    get_by_email, get_by_uid, validate_token
+)
