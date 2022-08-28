dd2873e967d6043f0da51b5b8016e9b2dfb38c8f
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-28 12:49:05 -0300

    test for service provider

--- sql_app/tests/test_service_provider.py
+++ sql_app/tests/test_service_provider.py
@@ -1,30 +1,55 @@
+import sys
+from urllib import response
+sys.path += [
+    "/Users/dilermando/dev/service_studio-backend/sql_app",
+    "/Users/dilermando/dev/service_studio-backend/sql_app/shared"
+]
+
 from fastapi.testclient import TestClient
-from fastapi import FastAPI
+from main import app
+from firebase_utils import get_user_by_email
 
-
-app = FastAPI()
 client = TestClient(app)
 
-def test_service_provider_create():
-    provider = {
-        "name": "test1",
-        "display_name": "Test Api",
-        "cpf": "123123123",
-        "email": "test1@example.com",
-        "phone_number": "123123123",
-        "signal": 0,
-        "description": "test api post studio"
-        }
+provider = {
+    "birth_date": "2022-08-25",
+    "display_name": "Test Api",
+    "cpf": "123123123",
+    "email": "test1@example.com",
+    "phone_number": "+55123123123",
+    "password": "testando1"
+}
 
+provider2 = {
+    "birth_date": "2022-08-25",
+    "cpf": "111111111",
+    "email": "test2@example.com",
+    "phone_number": "+551123123123",
+    "password": "testando1"
+}
+
+def test_withou_display_name():
+    response = client.post(
+        "/provider/create",
+        json = provider2
+    )
+    assert response.status_code == 422
+
+def test_create_correct():
     response = client.post(
         "/provider/create",
         json = provider
     )
     assert response.status_code == 201
-    assert response.json() == provider
+
+def test_create_duplicate():
+    response = client.post(
+        "/provider/create",
+        json = provider
+    )
+    assert response.status_code == 500
 
 def test_service_delete():
-    name = "test1"
-    response = client.get(f"/provider/remove/?name={name}")
+    user = get_user_by_email("test1@example.com")
+    response = client.get(f"/provider/remove/?uid={user.uid}")
     assert response.status_code == 200
-    assert response.json() == f"{name} deleted"
