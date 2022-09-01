ec50363a3cd743cd9c6373a4d9b3df8f31d42606
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-08-31 21:29:06 -0300

    added get_clients_unique_by_provider_email

--- sql_app/crud/crud_sell.py
+++ sql_app/crud/crud_sell.py
@@ -11,8 +11,14 @@ from schemas import SellCreate, SellInDBBase
 class CRUDSell(CRUDBase[Sell, SellCreate, SellInDBBase]):
     ...
 
-    def get_unique_by_provider_email(self, db: Session, service_provider_email: str) -> Optional[SellInDBBase]:
-        return db.query(self.model).filter(self.model.service_provider_name == service_provider_email).distinct(self.model.client_name).all()
+    def get_clients_unique_by_provider_email(self, db: Session, service_provider_email: str) -> Optional[SellInDBBase]:
+        sells = db.query(self.model).filter(self.model.service_provider_name == service_provider_email).distinct(self.model.client_name).all()
+        clients_email = []
+
+        for sell in sells:
+            clients_email.append(sell.client_name)
+
+        return clients_email
 
     def get_by_provider_email(self, db: Session, service_provider_email: str) -> Optional[SellInDBBase]:
         return db.query(self.model).filter(self.model.service_provider_name == service_provider_email).all()
