2be808f21081c2792ebec464a3873da99a2358df
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-09-08 18:11:50 -0300

    update url docker

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost/tattoo_python"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost/tattoo_python"
+SQLALCHEMY_DATABASE_URL = "postgresql://easeservice:easeservice@db:5432/tattoo_python"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
