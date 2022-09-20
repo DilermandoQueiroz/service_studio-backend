6168d818b0ad72b998ca42876220ee22dc15c1e9
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-09-20 19:59:23 -0300

    alembic dont work if we import Base from db

--- sql_app/migrations/env.py
+++ sql_app/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from db import Base
+from models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
