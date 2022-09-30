1e808d9302bffb98b8e63b657098b481e936b93b
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-09-30 17:37:20 -0300

    added validate token as dependencies

--- sql_app/routers/dependencies.py
+++ sql_app/routers/dependencies.py
@@ -1,4 +1,9 @@
+from fastapi import HTTPException, Request
 from db import SessionLocal
+from firebase_utils import validate_token
+from custom_logger import custom_logger
+
+logger = custom_logger(__name__)
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +11,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def validate_token_client(request: Request):
+    try:
+        user = validate_token(request.headers['authorization'])
+        if user:
+            return user
+    except Exception as error:
+        logger.error(error)
+        raise HTTPException(status_code=401, detail="You don't have access")
