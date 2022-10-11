918ac0ef9ca8186419f8dd22995b19dc9a46e82b
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-10-11 18:33:46 -0300

    added remove service provider

--- sql_app/routers/service_provider_route.py
+++ sql_app/routers/service_provider_route.py
@@ -3,10 +3,10 @@ from typing import List
 import crud
 import schemas
 from custom_logger import custom_logger
-from .dependencies import get_db
+from .dependencies import get_db, validate_token_client
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from firebase_utils import (create_service_provider_firebase,
-                            delete_by_user_uid, validate_token)
+                            delete_by_user_uid, get_user_by_email, validate_token)
 from sqlalchemy.orm import Session
 
 logger = custom_logger(__name__)
@@ -37,6 +37,9 @@ router = APIRouter(
 def create_service_provider(service_provider: schemas.ServiceProviderCreate, db: Session = Depends(get_db)):
     user = False
     try:
+        if crud.provider.get_by_email(db=db, email=service_provider.email):
+            raise HTTPException(status_code=422, detail=f"Email already registered")
+            
         user_firebase = schemas.ServiceProviderFireBase(
             display_name=service_provider.display_name,
             email=service_provider.email,
@@ -46,16 +49,13 @@ def create_service_provider(service_provider: schemas.ServiceProviderCreate, db:
 
         if user:
             db_service_provider_name = crud.provider.get_by_name(db=db, name=user.uid)
-            db_service_provider_email = crud.provider.get_by_email(db=db, email=service_provider.email)
             exceptions = []
 
             if db_service_provider_name:
                 exceptions.append("name")
-            if db_service_provider_email:
-                exceptions.append("email")
 
             if exceptions:
-                raise HTTPException(status_code=400, detail=f"{', '.join(exceptions)} already registered")
+                raise HTTPException(status_code=422, detail=f"{', '.join(exceptions)} already registered")
 
             user_db = schemas.ServiceProviderDB(
                 name=user.uid,
@@ -64,6 +64,8 @@ def create_service_provider(service_provider: schemas.ServiceProviderCreate, db:
             )
 
             crud.provider.create(db=db, obj_in=user_db)
+
+            return "ok"
             
     except Exception as error:
         logger.error(error)
@@ -74,7 +76,7 @@ def create_service_provider(service_provider: schemas.ServiceProviderCreate, db:
             if not db_service_provider_name:
                 delete_by_user_uid(user.uid)
 
-@router.get("/remove")
+@router.get("/remove2")
 def remove_service_provider_by_name(uid: str = None, db: Session = Depends(get_db)):
     try:
         response = crud.provider.remove_by_name(db=db, name=uid)
@@ -82,6 +84,24 @@ def remove_service_provider_by_name(uid: str = None, db: Session = Depends(get_d
             raise HTTPException(status_code=400, detail="Uid not exists")
         
         delete_by_user_uid(uid)
+
+        return response
+    except Exception as error:
+        logger.error(error)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+@router.get("/remove")
+def remove_service_provider_by_email(db: Session = Depends(get_db), user = Depends(validate_token_client)):
+    # TODO: remove sell with the service provider
+    try:
+        print("comecei")
+        crud.sell.delete_sell_by_email_service_provider(db=db, service_provider_email=user["email"])
+        response = crud.provider.remove_by_email(db=db, email=user["email"])
+
+        if not response:
+            raise HTTPException(status_code=400, detail="Uid not exists")
+        
+        delete_by_user_uid(user["user_id"])
 
         return response
     except Exception as error:
